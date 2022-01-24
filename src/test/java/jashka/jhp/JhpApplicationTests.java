8355db77e157ec37c0dfd2e4fb7cac38f5474d1c
Alexey Chernyadiev <achernyadiev@borlas.ru>
2022-01-24 16:49:12 +0300

    greeting работает

---
+++ src/test/java/jashka/jhp/JhpApplicationTests.java
@@ -0,0 +1,13 @@
+package jashka.jhp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
