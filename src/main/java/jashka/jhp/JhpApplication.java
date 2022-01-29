5a044890581a99e7289237041be5610dc723cc49
Alexey Chernyadiev <achernyadiev@borlas.ru>
2022-01-29 17:52:04 +0300

    - unused imports 1

--- src/main/java/jashka/jhp/JhpApplication.java
+++ src/main/java/jashka/jhp/JhpApplication.java
@@ -3,8 +3,6 @@ package jashka.jhp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Properties;
-
 @SpringBootApplication
 public class JhpApplication {
     public static void main(final String[] args) {
