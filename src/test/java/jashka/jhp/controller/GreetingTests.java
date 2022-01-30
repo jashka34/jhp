6337bbd644971a40b1204486a483152a292eab14
Alexey Chernyadiev <achernyadiev@borlas.ru>
2022-01-30 21:16:59 +0300

    t 4

--- src/test/java/jashka/jhp/controller/GreetingTests.java
+++ src/test/java/jashka/jhp/controller/GreetingTests.java
@@ -38,4 +38,18 @@ public class GreetingTests {
                 .andExpect(jsonPath("$.name").value("Hello, Hhh!"));
     }
 
+    @Test
+    void greeting1WithNameHhhJsonTest()  throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/greeting1?name=greet1").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Hello, greet1!"));
+    }
+
+    @Test
+    void greeting2WithNameHhhJsonTest()  throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/greeting2?name=greet2").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Hello, greet2!"));
+    }
+
 }
